39b88cd773463086c6d3825ee67faf3975c01077
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2013-08-14 23:59:44 +0200

    fixed positioning

--- app/controllers/TimeEntryController.scala
+++ app/controllers/TimeEntryController.scala
@@ -22,8 +22,47 @@ import play.api.libs.json.JsObject
  * Date: 19.07.13
  * Time: 13:21
  */
+    /*
+object DurationParser{
+  val p = "(\\d+)d\\s+(\\d+)h\\s+(\\d+)m\\s+(\\d+)s"r
+
+  /**
+   * Parses a duration string of the form "98d 01h 23m 45s" into milliseconds.
+   *
+   * @throws ParseException
+   */
+  public static long parseDuration(String duration) throws ParseException {
+    Matcher m = p.matcher(duration);
+
+    long milliseconds = 0;
+
+    if (m.find() && m.groupCount() == 4) {
+      int days = Integer.parseInt(m.group(1));
+      milliseconds += TimeUnit.MILLISECONDS.convert(days, TimeUnit.DAYS);
+      int hours = Integer.parseInt(m.group(2));
+      milliseconds += TimeUnit.MILLISECONDS
+        .convert(hours, TimeUnit.HOURS);
+      int minutes = Integer.parseInt(m.group(3));
+      milliseconds += TimeUnit.MILLISECONDS.convert(minutes,
+        TimeUnit.MINUTES);
+      int seconds = Integer.parseInt(m.group(4));
+      milliseconds += TimeUnit.MILLISECONDS.convert(seconds,
+        TimeUnit.SECONDS);
+    } else {
+      throw new ParseException("Cannot parse duration " + duration, 0);
+    }
+
+    return milliseconds;
+  }
+}  */
+
 object TimeEntryController extends Controller with GlobalDBAccess with securesocial.core.SecureSocial {
   val DefaultAccessRole = None
+
+  def parseAsDuration(s: String) = {
+    s
+    Some(4)
+  }
 
   def create(owner: String, repo: String, issueNumber: Int) = SecuredAction(ajaxCall = false, authorize = None, p = parse.urlFormEncoded) {
     implicit request =>
@@ -33,7 +72,7 @@ object TimeEntryController extends Controller with GlobalDBAccess with securesoc
         GithubApi.isCollaborator(user, user.githubAccessToken, fullName).map {
           case true =>
             (for {
-              duration <- postParameter("duration").flatMap(_.toIntOpt)
+              duration <- postParameter("duration").flatMap(parseAsDuration)
             } yield {
               val issue = Issue(fullName, issueNumber)
               val timeEntry = TimeEntry(issue, duration, "testUser")
