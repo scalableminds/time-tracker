5f3c597bdfabd5afda771f2453bff4aca43e2a84
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2014-07-31 12:27:14 +0200

    updated scm utils to 6.5.0

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ trait Dependencies{
   val akkaVersion = "2.2.0"
   val reactiveVersion = "0.10.0"
   val reactivePlayVersion = "0.10.2"
-  val scmUtilVersion= "6.2.0"
+  val scmUtilVersion= "6.5.0"
 
   val commonsIo = "commons-io" % "commons-io" % "2.4"
   val commonsEmail = "org.apache.commons" % "commons-email" % "1.3.1"
